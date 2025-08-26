bbec579d0a92e5f31b588c134e489cbaf8d6e777
joaojz289 <joaopauloelements@gmail.com>
2025-08-25 22:20:48 -0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,9 +1,7 @@
 export const PATH_NAVE_IMAGEM = "src/assets/imagens/nave.png";
 export const PATH_ALIEN_IMAGEM = "src/assets/imagens/alien.png";
 export const GameState={
-
     START: "Start",
     PLAYING: "Playing",
-    GAMER_OVER: "GamerOver"
-
+    GAME_OVER: "GamerOver"
 };
